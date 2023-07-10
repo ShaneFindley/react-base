49cd7875ad2911bdba84e970c4fd89320c789a1f
Shane Findley <shane.findley@sonarsource.com>
2023-07-10 10:24:18 +0200

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,10 @@
 import { Container } from '@mui/material'
 import Root from './react-router/roots'
 
+if(true) {
+  alert("Unexpected Condition");
+}
+
 export default function App () {
   return <div>
         <Container>
