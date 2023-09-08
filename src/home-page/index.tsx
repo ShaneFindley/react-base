b81eae41c8190702c57584a88690f07053df21c7
Virginia Friedrich <42647877+vfried@users.noreply.github.com>
2023-09-08 09:54:13 +0200

    Update index.tsx

--- src/home-page/index.tsx
+++ src/home-page/index.tsx
@@ -6,6 +6,12 @@ new s3.Bucket(this, 'id', {
     bucketName: 'bucket'
 }); // Sensitive
 
+const s32 = require('aws-cdk-lib/aws-s3');
+
+new s32.Bucket(this, 'id', {
+    bucketName: 'bucket'
+}); // Sensitive
+
 const HomePage = {
   path: '/',
   LazyRender: lazy(async () => await import('./home-page'))
